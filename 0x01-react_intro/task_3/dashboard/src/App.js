54bb35d433126717e7bfaaa856d4c8de3478b198
Maxrutto <ruttomaxmillan@gmail.com>
2025-02-03 18:41:40 +0300

    No update

---
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "./ALX_Logo.jpg";
+import "./App.css";
+import { getFullYear, getFooterCopy } from "./utils";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="ALX logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <div className="login-form">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" />
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+          <button>OK</button>
+        </div>
+      </div>
+      <footer className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </footer>
+    </div>
+  );
+}
+export default App;
