d2ac1c655a741fd847b5f0956490a50cc8202fbd
Maxrutto <ruttomaxmillan@gmail.com>
2025-02-04 16:26:27 +0300

    Tests

--- 0x01-react_intro/task_0/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_0/dashboard/src/setupTests.js
@@ -1,12 +1,12 @@
 import "@testing-library/jest-dom";
-import { configure } from "enzyme";
-import Adapter from "@cfaester/enzyme-adapter-react-18";
 
 // Add TextEncoder polyfill
 const { TextEncoder, TextDecoder } = require("util");
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
+import { configure } from "enzyme";
+import Adapter from "@cfaester/enzyme-adapter-react-18";
 // Enzyme configuration
 
 configure({ adapter: new Adapter() });
